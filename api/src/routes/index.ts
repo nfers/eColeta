ed704faeba1188887825242dfe1d21431712fb77
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-05 18:00:16 -0300

    ajustando método get da rota de items

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,28 +1,29 @@
 import { Router } from 'express';
+import knex from '../database/connection';
 
 
 const routes = Router();
 
+routes.get('/api/items', async (req, res) => {
 
-routes.get('/api', (req, res) => {
+  const items = await knex('items').select('*');
 
-  res.status(200).send({
-    result: true, data: ['API em execução']
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3030/api/uploads/${item.image}`,
+    };
+  });
+
+  return res.status(200).send({
+    result: true, data: [serializedItems]
   })
 });
-
-const users =
-  [
-    'Nayara',
-    'Mariana',
-    'Luna',
-    'Beatriz'
-  ];
 
 routes.get('/api/users', (req, res) => {
 
   res.status(200).send({
-    result: 'Ok', data: [users]
+    result: 'Ok', data: ['']
   })
 });
 
@@ -33,17 +34,17 @@ routes.get('api/users', (req, res) => {
   // const filterusers = search ? users.filter(user => user.includes((search))) : users; 
 
   res.status(200).send({
-    result: 'Ok', data: [users]
+    result: 'Ok', data: ['']
   })
 });
 
 routes.get('/api/users/:id', (req, res) => {
   const id = Number(req.params.id);
 
-  const user = users[id];
+  //const user = users[id];
 
   res.status(200).send({
-    result: 'Ok', data: [user]
+    result: 'Ok', data: ['user']
   })
 });
 
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 import routes from './routes/';
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+
+app.use('/api/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3030);
 
