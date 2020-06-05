12b73175f95b8567e8f01f8fcb2cee5dcb32bf5a
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-05 17:37:17 -0300

    ajuste nas migratios e inclusao do banco de dados

--- api/src/database/migrations/20200604210416_items.ts
+++ api/src/database/migrations/20200604210416_items.ts
@@ -1,10 +1,15 @@
 import * as Knex from "knex";
 
 
-export async function up(knex: Knex): Promise<any> {
+export async function up(knex: Knex) {
+  return knex.schema.createTable('items', table => {
+    table.increments('id').primary();
+    table.string('image').notNullable();
+    table.string('title').notNullable();
+  })
 }
 
-
-export async function down(knex: Knex): Promise<any> {
+export async function down(knex: Knex) {
+  return knex.schema.dropTable('items');
 }
 
