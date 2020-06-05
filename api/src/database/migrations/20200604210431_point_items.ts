87f60b3d3cb8af51e530c3270e106b682ad6b53a
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-05 17:47:56 -0300

    adicionando imagens e populando a tabela de 'items'

--- api/src/database/migrations/20200604210431_point_items.ts
+++ api/src/database/migrations/20200604210431_point_items.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex) {
 
     table.integer('point_id').notNullable().references('id').inTable('points');
     table.integer('item_id').notNullable().references('id').inTable('items');
-  })
+  });
 }
 
 export async function down(knex: Knex) {
