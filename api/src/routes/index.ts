747a174feff6a51672d764ee60e7611dc2520bdb
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-05 18:32:43 -0300

    realizado separação do metodo POST para o controller de Locais

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import knex from '../database/connection';
-
+import PointsController from './../controllers/PointsController';
 
 const routes = Router();
+
+const points = new PointsController();
 
 routes.get('/api/items', async (req, res) => {
 
@@ -32,45 +34,6 @@ routes.get('/api/users/:id', (req, res) => {
   })
 });
 
-routes.post('/api/points', async (req, res) => {
-  const {
-    name, 
-    email, 
-    whatsapp, 
-    latitude, 
-    longitude, 
-    city, 
-    uf, 
-    items
-  } = req.body;
-
-  const trx = await knex.transaction();
-
-  const insertedIds = await trx('points').insert({
-    image: 'null',
-    name, 
-    email, 
-    whatsapp, 
-    latitude, 
-    longitude, 
-    city, 
-    uf
-  });
-
-  const point_id = insertedIds[0]
-
-  const pointItems = items.map((item_id: number) => {
-    return {
-      item_id, 
-      point_id: point_id
-    };
-  });
-
-  await trx('point_items').insert(pointItems);
- 
-  return res.status(200).send({
-    result: true, data: [items]
-  });
-});
+routes.post('/api/points', points.create);
 
 export default routes;
