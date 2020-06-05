57846864cbf35b20b71ad939ab7a46dc7a1c96f3
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-04 21:46:19 -0300

    ajustando projeto web, removendo arquivos desnecessarios

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
+
+
 
 function App() {
   return (
     <div className="App">
+     <span> Eu sou a div</span>
      
     </div>
   );
