fc3283a5d45ad5fdad57ea647f9cceb829811dc2
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-06 21:49:03 -0300

    criacao dos componentes e paginas que serao utilizados no app

---
+++ web/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import './App.css';
+
+import Header from './components/Header';
+import Home from './pages/Home/';
+
+function App() {
+
+  return (
+    <div>
+      {/* <Header  title="eu sou o header" /> */}
+
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreatePoint = () => {
+	return (
+		<div>
+			<h1>Crie seu ponto de coleta</h1>
+		</div>
+	);
+}
+
+export default CreatePoint;
