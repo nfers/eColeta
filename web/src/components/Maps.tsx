b2d3369f86a821cd39bb9b0e40aaf96b60781978
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-07 19:07:42 -0300

    implementado funcao para inserir os pontos de coleta via api

--- web/src/components/Maps.tsx
+++ web/src/components/Maps.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet';
 
@@ -6,18 +6,26 @@ import { LeafletMouseEvent } from 'leaflet';
 const Maps = () => {
 
 	const [ selectedPosition, setSelectedPosition ] = useState<[number, number]>([0, 0]);
+	const [ initialPosition, setinitialPosition ] = useState<[number, number]>([0, 0]);
 	
 	function handleMapClick(event: LeafletMouseEvent) {
-		
 		setSelectedPosition([
 			event.latlng.lat,
 			event.latlng.lng
 		])
 	}
 
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(position => {
+			const { latitude, longitude } = position.coords;
+
+			setinitialPosition([latitude, longitude]);
+		})
+	}, []);
+
 	return (
 		<section id="maps">
-			<Map center={[-16.6698391, -49.2112945]} zoom={15} onClick={handleMapClick}>
+			<Map center={initialPosition} zoom={15} onClick={handleMapClick}>
 				<TileLayer
 					attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,12 @@
-import React, { useState, useEffect, ChangeEvent } from 'react';
+import React, { 
+	useState, 
+	useEffect, 
+	ChangeEvent, 
+	FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 
 import Maps from './../../components/Maps';
 
@@ -32,6 +38,13 @@ const CreatePoint = () => {
 	const [city, setCity] = useState<string[]>([]);
 	const [ufSelected, setUfSelected] = useState('0');
 	const [cityfSelected, setCitySelected] = useState('0');
+	const [ selectedItems, setSelectedItems ] = useState<number[]>([]);
+
+	const [ formData, setFormData ] = useState({
+		name: '', 
+		email: '',
+		whatsapp: '',
+	});
 
 	useEffect(() => {
 		api.get('items').then(response => {
@@ -68,6 +81,67 @@ const CreatePoint = () => {
 		const city = event.target.value;
 
 		setCitySelected(city)
+	};
+
+	function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+		const { name, value } = event.target;
+
+		setFormData({ ...formData, [name]: value})
+	};
+
+	function handleSelectItem(id: number) {
+		const itemSelected = selectedItems.findIndex(item => item === id);
+
+		if (itemSelected >= 0) {
+			const filteredItems = selectedItems.filter(item => item !== id);
+			setSelectedItems(filteredItems);
+		} else {
+		setSelectedItems([...selectedItems, id]);
+		}
+	}
+
+	const [ selectedPosition, setSelectedPosition ] = useState<[number, number]>([0, 0]);
+	const [ initialPosition, setinitialPosition ] = useState<[number, number]>([0, 0]);
+	
+	function handleMapClick(event: LeafletMouseEvent) {
+		setSelectedPosition([
+			event.latlng.lat,
+			event.latlng.lng
+		])
+	}
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(position => {
+			const { latitude, longitude } = position.coords;
+
+			setinitialPosition([latitude, longitude]);
+		})
+	}, []);
+
+
+	async function handleSubmit(event: FormEvent) {
+		event.preventDefault();
+
+		const { name , email, whatsapp } = formData;
+		const uf = ufSelected;
+		const city = cityfSelected;
+
+		const [ latitude, longitude ] = selectedPosition;
+		const items = selectedItems;
+		const data = {
+			name, 
+			email, 
+			whatsapp, 
+			city, 
+			uf,
+			latitude, 
+			longitude, 
+			items
+		}
+		await api.post('points', data);
+
+		alert('Ponto de Coleta Criado')
+		
 	}
 
 	return (
@@ -81,7 +155,7 @@ const CreatePoint = () => {
 				</Link>
 			</header>
 
-			<form>
+			<form onSubmit={handleSubmit}>
 				<h1>
 					Cadastro do
 					<br />Ponto de Coleta
@@ -95,18 +169,18 @@ const CreatePoint = () => {
 
 				<div className="field">
 					<label htmlFor="name">Nome da Entidade</label>
-					<input type="text" name="name" id="name" />
+					<input type="text" name="name" id="name" onChange={handleInputChange}/>
 				</div>
 
 				<div className="field-group">
 					<div className="field">
 						<label htmlFor="email">E-mail</label>
-						<input type="email" name="email" id="email" />
+						<input type="email" name="email" id="email" onChange={handleInputChange}/>
 					</div>
 
 					<div className="field">
 						<label htmlFor="whatsapp">Whatsapp</label>
-						<input type="text" name="whatsapp" id="whatsapp" />
+						<input type="text" name="whatsapp" id="whatsapp" onChange={handleInputChange} />
 					</div>
 
 				</div>
@@ -117,7 +191,14 @@ const CreatePoint = () => {
 						<span>Selecione o Endereço no Mapa</span>
 					</legend>
 
-					<Maps />
+					<section id="maps">
+			<Map center={initialPosition} zoom={15} onClick={handleMapClick}>
+				<TileLayer
+					attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+				<Marker position={selectedPosition} />
+			</Map>
+		</section>
 
 					<div className="field-group">
 						<div className="field">
@@ -159,7 +240,11 @@ const CreatePoint = () => {
 
 					<ul className="items-grid">
 						{items.map(item => (
-							<li className="selected" key={item.id}>
+							<li 
+								className={selectedItems.includes(item.id) ? 'selected' : ''}
+								key={item.id} 
+								onClick={() => handleSelectItem(item.id)}
+							>							
 								<img src={item.image_url} alt={item.title} />
 								<span key={item.id}>{item.title}</span>
 							</li>
