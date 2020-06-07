ccd2930968c1a1ef3a28535bc729623b5d39da43
Nayara Ferreira <ncy.fers@gmail.com>
2020-06-07 14:07:15 -0300

    adicionado chamada da  api para listar os itens de coleta

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
+import api from '../../services/api';
 
 import './style.css';
 import logo from '../../assets/logo.svg';
 
+interface Item {
+	id: number;
+	title: string;
+	image_url: string;
+}
+
 const CreatePoint = () => {
+
+	const [items, setItems] = useState<Item[]>([]);
+
+	useEffect(() => {
+		api.get('items').then(response => {			
+			setItems(response.data.data[0])
+		});
+	}, []);
+
 	return (
 		<div id="page-create-point">
 			<header>
@@ -83,18 +99,13 @@ const CreatePoint = () => {
 					</legend>
 
 					<ul className="items-grid">
-						<li className="selected">
-							<img src="http://localhost:3030/api/uploads/lampadas.svg" alt="teste" />
-							<span>Lâmpadas</span>
-						</li>
-						<li className="">
-							<img src="http://localhost:3030/api/uploads/oleo.svg" alt="teste" />
-							<span>Óleo</span>
-						</li>
-						<li className="">
-							<img src="http://localhost:3030/api/uploads/baterias.svg" alt="teste" />
-							<span>Óleo</span>
-						</li>
+	{items.map(item => (
+		<li className="selected" key={item.id}>
+		<img src={item.image_url} alt="teste" />
+		<span key={item.id}>{item.title}</span>
+	</li>
+	))}
+					
 					</ul>
 
 				</fieldset>
